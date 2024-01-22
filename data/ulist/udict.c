87ea43d5121d027f0d67ba8ad48fd36535c4aa85
Ellu <lutaoyu518@gmail.com>
2024-01-23 04:15:42 +0800

    Add udict.c and udict.h files for implementing a dynamic dictionary data structure.

---
+++ data/ulist/udict.c
@@ -0,0 +1,207 @@
+/**
+ * @file udict.c
+ * @brief 数据类型通用的动态字典实现
+ * @author Ellu (ellu.grif@gmail.com)
+ * @version 1.0
+ * @date 2024-01-23
+ *
+ * THINK DIFFERENTLY
+ */
+
+#include "udict.h"
+
+#include <string.h>
+
+#define _udict_memmove memmove
+#define _udict_memcpy memcpy
+#define _udict_memset memset
+#define _udict_malloc m_alloc
+#define _udict_free m_free
+#define _udict_realloc m_realloc
+#define _udict_memcmp memcmp
+
+static void udict_elfree(void* ptr) {
+  udict_node_t* node = (udict_node_t*)ptr;
+  if (node->dynamic_value) {
+    _udict_free(node->value);
+  }
+}
+
+bool udict_init(UDICT dict) {
+  if (!ulist_init(&dict->nodes, sizeof(udict_node_t), 0, NULL, udict_elfree)) {
+    return false;
+  }
+  dict->size = 0;
+  dict->iter = 0;
+  return true;
+}
+
+UDICT udict_create(void) {
+  UDICT dict = (UDICT)_udict_malloc(sizeof(udict_t));
+  if (!dict) {
+    return NULL;
+  }
+  if (!udict_init(dict)) {
+    _udict_free(dict);
+    return NULL;
+  }
+  return dict;
+}
+
+void udict_clear(UDICT dict) {
+  ulist_clear(&dict->nodes);
+  dict->size = 0;
+}
+
+void udict_free(UDICT dict) {
+  udict_clear(dict);
+  _udict_free(dict);
+}
+
+static inline uint8_t fast_strcmp(const char* str1, const char* str2) {
+  if (str1 == str2) return 1;
+  while ((*str1) && (*str2)) {
+    if ((*str1++) != (*str2++)) return 0;
+  }
+  return (!*str1) && (!*str2);
+}
+
+static udict_node_t* udict_find_node(UDICT dict, const char* key) {
+  if (!dict || !key || !dict->size) {
+    return NULL;
+  }
+  ulist_foreach(&dict->nodes, udict_node_t, node) {
+    if (fast_strcmp(node->key, key)) {
+      return node;
+    }
+  }
+  return NULL;
+}
+
+bool udict_has_key(UDICT dict, const char* key) {
+  return udict_find_node(dict, key) != NULL;
+}
+
+void* udict_get(UDICT dict, const char* key) {
+  udict_node_t* node = udict_find_node(dict, key);
+  return node ? node->value : NULL;
+}
+
+const char* udict_get_reverse(UDICT dict, void* value) {
+  if (!dict || !value || !dict->size) {
+    return NULL;
+  }
+  ulist_foreach(&dict->nodes, udict_node_t, node) {
+    if (node->value == value) {
+      return node->key;
+    }
+  }
+  return NULL;
+}
+
+static inline void udict_internal_modify(udict_node_t* node, void* value,
+                                         uint8_t dyn) {
+  if (node->dynamic_value) _udict_free(node->value);
+  node->value = value;
+  node->dynamic_value = dyn;
+}
+
+bool udict_set(UDICT dict, const char* key, void* value) {
+  udict_node_t* node = udict_find_node(dict, key);
+  if (node) {
+    udict_internal_modify(node, value, 0);
+    return true;
+  } else {
+    udict_node_t* node = (udict_node_t*)ulist_append(&dict->nodes);
+    if (!node) return false;
+    node->key = key;
+    node->value = value;
+    node->dynamic_value = 0;
+    dict->size++;
+    return true;
+  }
+}
+
+bool udict_set_copy(UDICT dict, const char* key, void* value, size_t size) {
+  void* buf = _udict_malloc(size);
+  _udict_memcpy(buf, value, size);
+  if (!buf) return false;
+  udict_node_t* node = udict_find_node(dict, key);
+  if (node) {
+    udict_internal_modify(node, buf, 1);
+    return true;
+  } else {
+    udict_node_t* node = (udict_node_t*)ulist_append(&dict->nodes);
+    if (!node) {
+      _udict_free(buf);
+      return false;
+    }
+    node->key = key;
+    node->value = buf;
+    node->dynamic_value = 1;
+    dict->size++;
+    return true;
+  }
+}
+
+void* udict_set_alloc(UDICT dict, const char* key, size_t size) {
+  void* buf = _udict_malloc(size);
+  if (!buf) return NULL;
+  udict_node_t* node = udict_find_node(dict, key);
+  if (node) {
+    udict_internal_modify(node, buf, 1);
+    return buf;
+  } else {
+    udict_node_t* node = (udict_node_t*)ulist_append(&dict->nodes);
+    if (!node) {
+      _udict_free(buf);
+      return false;
+    }
+    node->key = key;
+    node->value = buf;
+    node->dynamic_value = 1;
+    dict->size++;
+    return buf;
+  }
+}
+
+bool udict_delete(UDICT dict, const char* key) {
+  udict_node_t* node = udict_find_node(dict, key);
+  if (!node) return false;
+  if (node->dynamic_value) {
+    _udict_free(node->value);
+  }
+  ulist_remove(&dict->nodes, node);
+  dict->size--;
+  return true;
+}
+
+void* udict_pop(UDICT dict, const char* key, uint8_t* should_free) {
+  udict_node_t* node = udict_find_node(dict, key);
+  if (!node) {
+    if (should_free) *should_free = 0;
+    return NULL;
+  }
+  void* value = node->value;
+  if (should_free) *should_free = node->dynamic_value;
+  ulist_remove(&dict->nodes, node);
+  dict->size--;
+  return value;
+}
+
+bool udict_iter(UDICT dict, const char** key, void** value) {
+  if (!dict || !dict->size) {
+    return false;
+  }
+  if (dict->iter >= dict->size) {
+    dict->iter = 0;
+    if (key) *key = NULL;
+    if (value) *value = NULL;
+    return false;
+  }
+  udict_node_t* node = (udict_node_t*)ulist_get(&dict->nodes, dict->iter);
+  if (key) *key = node->key;
+  if (value) *value = node->value;
+  dict->iter++;
+  return true;
+}
---
+++ data/ulist/udict.h
@@ -0,0 +1,148 @@
+/**
+ * @file udict.h
+ * @brief 数据类型通用的动态字典实现
+ * @author Ellu (ellu.grif@gmail.com)
+ * @version 1.0
+ * @date 2024-01-23
+ *
+ * THINK DIFFERENTLY
+ */
+
+#ifndef __UDICT_H__
+#define __UDICT_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "modules.h"
+#include "ulist.h"
+
+typedef uint16_t udict_size_t;
+
+#pragma pack(1)
+typedef struct udict_node {
+  const char* key;
+  void* value;
+  uint8_t dynamic_value;
+} udict_node_t;
+
+typedef struct udict {
+  ulist_t nodes;
+  udict_size_t size;
+  udict_size_t iter;
+} udict_t;
+
+typedef udict_t* UDICT;
+#pragma pack()
+
+/**
+ * @brief 初始化一个字典
+ * @param dict 字典
+ *
+ * @return 是否成功
+ */
+extern bool udict_init(UDICT dict);
+
+/**
+ * @brief 创建一个字典
+ *
+ * @return 字典，如果失败则为NULL
+ */
+extern UDICT udict_create(void);
+
+/**
+ * @brief 清空字典
+ * @param dict 字典
+ */
+extern void udict_clear(UDICT dict);
+
+/**
+ * @brief 释放字典
+ * @param dict 字典
+ */
+extern void udict_free(UDICT dict);
+
+/**
+ * @brief 获取字典大小
+ * @param dict 字典
+ *
+ * @return 字典大小
+ */
+static inline udict_size_t udict_size(UDICT dict) { return dict->size; }
+
+/**
+ * @brief 检查字典是否包含某个键
+ * @param  dict     字典
+ * @param  key      键
+ * @retval true     包含
+ */
+extern bool udict_has_key(UDICT dict, const char* key);
+
+/**
+ * @brief 获取字典中的值
+ * @param  dict     字典
+ * @param  key      键
+ * @retval value    值，如果不存在则为NULL
+ */
+extern void* udict_get(UDICT dict, const char* key);
+
+/**
+ * @brief 获取字典中的键
+ * @param  dict     字典
+ * @param  value    值
+ * @retval key      键，如果不存在则为NULL
+ */
+extern const char* udict_get_reverse(UDICT dict, void* value);
+
+/**
+ * @brief 设置字典中的值
+ * @param  dict     字典
+ * @param  key      键
+ * @param  value    值
+ * @retval true     成功
+ */
+extern bool udict_set(UDICT dict, const char* key, void* value);
+
+/**
+ * @brief 从缓冲区复制数据以设置字典中的值
+ * @param  dict     字典
+ * @param  key      键
+ * @param  value    值
+ * @param  size     大小
+ * @retval true     成功
+ */
+extern bool udict_set_copy(UDICT dict, const char* key, void* value,
+                           size_t size);
+
+/**
+ * @brief 分配一片新内存给指定的值并返回
+ * @param  dict     字典
+ * @param  key      键
+ * @param  size     大小
+ * @retval ptr      值指针，如果失败则为NULL
+ */
+extern void* udict_set_alloc(UDICT dict, const char* key, size_t size);
+
+/**
+ * @brief 删除字典中的项目
+ * @param  dict     字典
+ * @param  key      键
+ * @retval true     成功
+ */
+extern bool udict_delete(UDICT dict, const char* key);
+
+/**
+ * @brief 弹出字典中的项目
+ * @param  dict         字典
+ * @param  key          键
+ * @param  should_free  该值域是否需要手动释放
+ * @retval value        值，如果不存在则为NULL
+ */
+extern void* udict_pop(UDICT dict, const char* key, uint8_t* should_free);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __UDICT_H__
