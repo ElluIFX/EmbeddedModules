e011633d708c45dde0aa01cb8928e8fff250c951
Ellu <lutaoyu518@gmail.com>
2024-06-01 19:20:55 +0800

    ✨ feat(log): custom include in kconfig

--- debug/log/log.h
+++ debug/log/log.h
@@ -21,6 +21,10 @@ extern "C" {
 #include "uni_io.h"
 #endif
 
+#if LOG_CFG_CUSTOM_INCLUDE_ENABLE
+#include LOG_CFG_CUSTOM_INCLUDE_FILE
+#endif
+
 #define LOG_LEVEL_TRACE 0
 #define LOG_LEVEL_DEBUG 1
 #define LOG_LEVEL_PASS 2
